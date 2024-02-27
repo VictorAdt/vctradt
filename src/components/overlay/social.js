e7174196a205b8f67d85c8f5ccbf1542d5cdf7ba
victor audetat <doubleo.vk@gmail.com>
2024-02-27 22:22:49 +0100

    add socials link

--- src/components/overlay/social.js
+++ src/components/overlay/social.js
@@ -1,80 +1,91 @@
 import { animated, useSpring, useSprings, useTrail } from "@react-spring/web";
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
-
+const socials = [
+    {
+        name: 'Linkedin',
+        url: '',
+        icon: faLinkedinIn,
+    },
+    {
+        name: 'Github',
+        url: 'https://github.com/VictorAdt',
+        icon: faGithub,
+    },
+    {
+        name: 'Mail',
+        url: '#',
+        icon: faEnvelope,
+    },
+]
+const icongap = 6
 const iconSize = '40px'
 const containerpadding = 10
 const iconSizeN = 40
-const iconSpacing = 2
 
 export const Social = () => {
 
-    const socials = [
-        {
-            name: 'Linkedin',
-            url: '',
-            icon: '',
-        },
-        {
-            name: 'Github',
-            url: '',
-            icon: '',
-        },
-    ]
-
     const [isClicked, setIsClicked] = useState(false)
-
+    const springConfig = { mass: 4, tension: 3000, friction: 100 }
     const { height } = useSpring({
-        height: isClicked ? `${((socials.length * iconSizeN) + (socials.length) * iconSpacing) + containerpadding * 2}px` : `${iconSizeN}px`,
-        config: { mass: 4, tension: 3000, friction: 100 },
-    })
-
-    const [buttonSprings] = useSprings(socials.length, i => {
-        console.log(i);
-        const y = isClicked ? i * 30 : 0
-        return ({
-            from: { y: 0 },
-            to: { y: isClicked ? i * 30 : 0 },
-        })
+        height: isClicked ? (socials.length * iconSizeN) + (socials.length * icongap) + (containerpadding * 4) : iconSizeN,
+        config: springConfig,
     })
 
     const [springs, api] = useSprings(
         socials.length,
-        (i) => ({
-
-            y: isClicked ? i * -50 : 0
+        () => ({
+            y: 0,
+            config: springConfig,
         }),
     )
+
+    const handleClick = () => {
+        setIsClicked(!isClicked)
+        api.start(i => ({
+            y: !isClicked ? (i + 1) * -iconSizeN - icongap * (i + 1) : 0,
+        }))
+    }
 
     return (
         <FloatingButtonContainer
             style={{ height }}
-            onClick={() => setIsClicked(!isClicked)}
         >
-            <FloatingButton>
+            <FloatingButton onClick={handleClick}>
                 {Array.from({ length: 3 }).map(() => <Dot />)}
             </FloatingButton>
-            <ItemsContaier>
+            <ItemsContaier active={isClicked}>
                 {springs.map((props, index) => {
                     return (
-                        <FloatingButton style={props} key={index} />
+                        <FloatingButtonLink
+                            style={props}
+                            active={isClicked}
+                            key={index}
+                            href={socials[index].url}
+                            rel="noopener noreferrer"
+                            target={"_blank"}
+                        >
+                            <FontAwesomeIcon icon={socials[index].icon} />
+                        </FloatingButtonLink>
                     )
                 })}
             </ItemsContaier>
-        </FloatingButtonContainer>
+        </FloatingButtonContainer >
     );
 }
 
 const ItemsContaier = styled('div')`    
     width: ${iconSize};
-    height: ${iconSize};
+    height: ${props => props.active ? (socials.length * iconSizeN) + (socials.length * icongap) + (containerpadding * 4) : iconSizeN}px;
     position: relative;
-    z-index: 0;
+    z-index: ${props => props.active ? 'initial' : 1};
 `
 
-
-const FloatingButton = styled(animated.div)`
+const FloatingButton = styled(animated.button)`
     width: ${iconSize};
     height: ${iconSize};
     background-color: #fff;
@@ -85,7 +96,24 @@ const FloatingButton = styled(animated.div)`
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    border: 0;
 `
+
+const FloatingButtonLink = styled(animated.a)`
+    width: ${iconSize};
+    font-size: 18px;
+    height: ${iconSize};
+    background-color: #fff;
+    border-radius: 30px;
+    z-index: ${props => props.active ? 200 : 99};
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    flex-direction: column;
+`
+
 
 const FloatingButtonContainer = styled(animated.div)`
     border-radius: 50px;
@@ -95,6 +123,7 @@ const FloatingButtonContainer = styled(animated.div)`
     left: 40px;
     border: 1px solid #333;
     padding: 10px;
+    z-index: 0;
     display: flex;
     align-items: flex-end;
     justify-content: center;
